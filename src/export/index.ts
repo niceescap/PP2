c8954624d4218daf50f613994bac693f8887c9c8
niceescap <niceescap@gmail.com>
2026-07-29 19:50:59 +0200

    feat: export WebCodecs exporter

--- src/export/index.ts
+++ src/export/index.ts
@@ -1,2 +1,4 @@
 export { VideoExporter } from './media-recorder.js';
 export type { ExportOptions } from './media-recorder.js';
+export { WebCodecsExporter, isWebCodecsSupported } from './webcodecs-exporter.js';
+export type { WebCodecsExportOptions } from './webcodecs-exporter.js';
